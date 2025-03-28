b1eda6db18820448e60b21d82708440b5a172d90
DilanHansaja <dilanhansaja81@gmail.com>
2025-03-28 15:07:19 +0530

    Final

---
+++ file_organizer.py
@@ -0,0 +1,127 @@
+from pathlib import Path
+import shutil
+import os
+
+media_extensions = {
+        'images': ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff'],
+        'videos': ['.mp4', '.avi', '.mkv', '.mov', '.flv', '.wmv'],
+        'audios': ['.mp3', '.wav', '.aac', '.flac', '.ogg'],
+        'documents': ['.pdf', '.doc', '.docx', '.pptx'],
+        'other': ['.psd', '.ai', '.eps']
+    }
+
+duplicates=[]
+
+folders = {"Images":None,"Videos":None,"Audios":None,"Documents":None,"Other":None}
+
+def scanDirectory(directory_path):
+
+    path = Path(directory_path)
+
+    number_of_files = len(os.listdir(directory_path))
+
+    if number_of_files > 0 :
+
+        increment = 100 / number_of_files
+
+        percentage = 0
+
+        for file in path.iterdir():
+
+            scanned_file = Path(file)
+
+
+            if scanned_file.suffix in media_extensions["images"]:
+
+                create_folder("Images", directory_path, scanned_file)
+
+            elif scanned_file.suffix in media_extensions["videos"]:
+
+                create_folder("Videos", directory_path, scanned_file)
+
+            elif scanned_file.suffix in media_extensions["audios"]:
+
+                create_folder("Audios", directory_path, scanned_file)
+
+            elif scanned_file.suffix in media_extensions["documents"]:
+
+                create_folder("Documents", directory_path, scanned_file)
+
+            else:
+
+                create_folder("Other", directory_path, scanned_file)
+
+            percentage += increment
+            print(f'Progress: {int(percentage)}%', end='\r')
+
+        print(f'Completed: 100%', end="\r")
+
+        checkDuplicates()
+
+    else:
+        print("Provided directory is empty!")
+
+def create_folder(folder_name,directory_path,scanned_file):
+
+    if folders.get(folder_name) is None:
+
+        folders[folder_name]= Path(directory_path,folder_name)
+        folders[folder_name].mkdir(0o777, True, True)
+
+        moveFile(scanned_file, folders[folder_name])
+
+    else:
+        moveFile(scanned_file, folders[folder_name])
+
+
+def moveFile(source,destination):
+
+    def check_if_already_exists():
+
+        check_path = Path(destination,source.name)
+
+        if check_path.exists():
+
+           duplicates.append(source)
+
+        else:
+            shutil.move(source,destination)
+            #print(f'{source.name} is moved to {destination}')
+
+    check_if_already_exists()
+
+def checkDuplicates():
+
+    if len(duplicates) >0:
+        input(f'{len(duplicates)} Duplicate files were found.\nThey are not affected by the program.')
+
+def initialize():
+
+    while True:
+
+        provided_path = input('Enter the directory path  to organize files : ')
+        print()
+        confirmation = input('Please make sure your path have only files in it.\nIf folders found they will move to the folder named "Other" after organized.\nContinue (y/n) :').lower()
+        print()
+
+        if confirmation.__eq__("y"):
+
+            path = Path(provided_path)
+            if path.exists():
+                scanDirectory(provided_path)
+                print()
+                break
+            else:
+                print("Invalid path!")
+                print()
+                continue
+
+        elif confirmation.__eq__("n"):
+            print("Thank you for using file organizer!")
+            break
+        else:
+            print("Invalid Command!")
+            print()
+            continue
+
+initialize()
